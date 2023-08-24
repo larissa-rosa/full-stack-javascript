6b91b331ecdea3921267553db4ebb14775a427f0
Larissa Rosa <larissa_rosa7@hotmail.com>
2023-08-24 10:59:56 -0300

    Usando classes para o DOM

---
+++ 40-classes-dom/index.js
@@ -0,0 +1,30 @@
+import { Component } from "./Component.js";
+
+import { Form } from "./Form.js";
+
+import { Input } from "./Input.js";
+
+import { Label } from "./Label.js";
+
+const el = new Component('h1', 'body', { innerText: 'Olá, mundo!' })
+console.log(el)
+
+el.tag = 'h2'
+el.build().render()
+
+const form = new Form('body')
+
+const label = new Label('Nome:', form, { htmlFor: 'nameInput'})
+const input = new Input(form, { id: 'nameInput', name: 'birthday'})
+
+form.render()
+label.render()
+
+form.addChildren(input)
+
+form.addChildren(
+  new Component('br'),
+  new Component('br'),
+  new Label('Data da nascimento:', { htmlFor: 'birthdayInput'}),
+  new Input(form, { id: 'birthdayInput', name: 'birthday', type: 'date'})
+)
